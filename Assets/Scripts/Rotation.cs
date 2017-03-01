414850f2104ac4a41ac4d2fc5e29a6f2675a8045
井上大 <ddorange55@gmail.com>
2017-03-01 11:19:45 +0900

    add file

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+
+	[SerializeField] private bool isRotation = true;
+	[SerializeField] private float speed = 30.0f;
+
+	void Update () {
+
+		if (isRotation) {
+			Vector3 r = transform.rotation.eulerAngles;
+			r.y += speed * Time.deltaTime;
+			transform.rotation = Quaternion.Euler(r);
+		}
+		
+	}
+}
